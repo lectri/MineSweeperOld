4e5c96befc96fff048336e202ccd973f2fdea02a
lectri <cmade2006@gmail.com>
2021-05-28 19:04:50 -0400

    Converted to OOP

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pyglet
+import random
 from pyglet.gl import glClearColor
+from pyglet.window import mouse
 
 # Window Variables
 WIDTH = 720
@@ -8,19 +10,57 @@ caption = "MineSweeper"
 
 # Create Window & Batch
 window = pyglet.window.Window(WIDTH, HEIGHT, caption=caption)
-batch = pyglet.graphics.Batch()
+rectangle_batch = pyglet.graphics.Batch()
+line_batch = pyglet.graphics.Batch()
 # Change Background
 glClearColor(128, 128, 128, 1.0)
+
+buttons = []
+# Sprites
+
+
+class Rectangle:
+    def __init__(self, x, y, has_bomb):
+        self.has_bomb = has_bomb
+        self.hidden = True
+
+        self.sprite = pyglet.shapes.Rectangle(
+            x, y, width=60, height=40, color=(128, 128, 128), batch=rectangle_batch)
+
+    def on_click(self):
+        self.hidden = False
+        pass
+
+
+buttons = []
+lines = []
+
+
+class Grid(Rectangle):
+    def create_grid(*self):
+        # Create Rectangles
+        for height in range(0, 8*60, 40):
+            for width in range(0, 12*60, 60):
+                buttons.append(
+                    Rectangle(width, height, random.choice([True, False]))
+                )
+        for i in range(0, WIDTH, 60):
+            lines.append(pyglet.shapes.Line(i, 0, i, HEIGHT,
+                         width=4, color=(0, 0, 0), batch=line_batch))
+            for i in range(0, HEIGHT, 60):
+                lines.append(pyglet.shapes.Line(0, i, WIDTH, i,
+                             width=4, color=(0, 0, 0), batch=line_batch))
 
 
 @window.event
 def on_draw():
     window.clear()
-    batch.draw()
+    rectangle_batch.draw()
+    line_batch.draw()
 
 
-@window.event()
-def on_mouse_press():
+@window.event
+def on_mouse_press(x, y, button, modifiers):
     pass
 
 
@@ -28,31 +68,7 @@ def update():
     pass
 
 
-buttons = []
-lines = []
-
-
-def create_visual_grid():
-    # Create Rectangles
-    for height in range(0, 8*60, 40):
-        for width in range(0, 12*60, 60):
-            buttons.append(pyglet.shapes.Rectangle(
-                width, height, width=60, height=40, color=(128, 128, 128), batch=batch))
-
-    # Create Grid Lines, so player can tell which button is which
-    for i in range(0, WIDTH, 60):
-        lines.append(pyglet.shapes.Line(i, 0, i, HEIGHT,
-                     width=4, color=(0, 0, 0), batch=batch))
-    for i in range(0, HEIGHT, 60):
-        lines.append(pyglet.shapes.Line(0, i, WIDTH, i,
-                     width=4, color=(0, 0, 0), batch=batch))
-
-    dimensions = [12, 8]
-    return dimensions
-
-
-# Setup for Game Loop
-dimensions = create_visual_grid()
+Grid.create_grid()
 
 
 if __name__ == "__main__":
